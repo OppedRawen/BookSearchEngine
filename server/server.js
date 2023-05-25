717e91b4d69f5bf41dafaf8ac533b962ef188f32
OppedRawen <yud2372@gmail.com>
2023-05-25 01:33:14 -0700

    typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -22,7 +22,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,"../client/build/index.html"));
 
+})
+const startAplloServer =async()=>{
+  await server.start();
+  server.applyMiddleware({app});
+}
 app.use(routes);
 
 db.once('open', () => {
