c0b0d541b698090301d87b2b0e03e7a51fef221c
OppedRawen <yud2372@gmail.com>
2023-06-24 22:54:56 -0700

    setContext

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,26 @@ import {
   InMemoryCache,
   ApolloClient,
   createHttpLink} from '@apollo/client';
+  import{setContext} from '@apollo/client/link/context';
 
 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 const httpLink = createHttpLink({
-  url:'/graphql'
+  url:'https://googlesearchbook.herokuapp.com/graphql'
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 const client = new ApolloClient({
-  link:httpLink,
+  link:authLink.concat(httpLink),
   cache:new InMemoryCache()
 })
 function App() {
